fd84d9aae78cc3b4b73ddbeb05c2774dba71204d
Riya Dattani <riya_dattani@hotmail.com>
2019-04-04 15:55:35 +0100

    added power saving mode feature

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -7,70 +7,52 @@ describe("Thermostat", function() {
   });
 
   it("starts with 20 degrees", function() {
-    expect(thermostat.temperature).toEqual(20);
+    expect(thermostat.currentTemperature()).toEqual(20);
+  });
+
+  it("should increase the temperature using up", function() {
+    thermostat.up()
+    expect(thermostat.currentTemperature()).toEqual(21);
+  });
+
+  it("should increase the temperature using down", function() {
+    thermostat.down()
+    expect(thermostat.currentTemperature()).toEqual(19);
+  });
+
+  it("should have a minmum of 10 degrees", function() {
+    for (i = 1; i < 11; i++) {
+      thermostat.down()
+    }
+    expect(thermostat.currentTemperature()).toEqual(10);
+    thermostat.down()
+    expect(thermostat.currentTemperature()).toEqual(10);
+  });
+
+  describe("psMode", function() {
+    it("psMode is true by default", function() {
+      expect(thermostat.psMode()).toBe(true)
+    });
+
+    it("thermostat can be turned off", function() {
+      thermostat.switch();
+      expect(thermostat.psMode()).toBe(false)
+    });
+
+    it("there is a maximum temp of 25 when psMode is true", function () {
+      // thermostat.psMode()
+      for (i = 1; i < 10; i++) {
+        thermostat.up()
+      }
+      expect(thermostat.currentTemperature()).toEqual(25)
+    });
+
+    it("there is a maximum temp of 32", function () {
+      thermostat.switch()
+      for (i = 1; i < 15; i++) {
+        thermostat.up()
+      }
+      expect(thermostat.currentTemperature()).toEqual(32)
+    });
   });
 });
-
-
-
-
-
-
-// describe("Player", function() {
-//   var player;
-//   var song;
-//
-//   beforeEach(function() {
-//     player = new Player();
-//     song = new Song();
-//   });
-//
-//   it("should be able to play a Song", function() {
-//     player.play(song);
-//     expect(player.currentlyPlayingSong).toEqual(song);
-//
-//     //demonstrates use of custom matcher
-//     expect(player).toBePlaying(song);
-//   });
-//
-//   describe("when song has been paused", function() {
-//     beforeEach(function() {
-//       player.play(song);
-//       player.pause();
-//     });
-//
-//     it("should indicate that the song is currently paused", function() {
-//       expect(player.isPlaying).toBeFalsy();
-//
-//       // demonstrates use of 'not' with a custom matcher
-//       expect(player).not.toBePlaying(song);
-//     });
-//
-//     it("should be possible to resume", function() {
-//       player.resume();
-//       expect(player.isPlaying).toBeTruthy();
-//       expect(player.currentlyPlayingSong).toEqual(song);
-//     });
-//   });
-//
-//   // demonstrates use of spies to intercept and test method calls
-//   it("tells the current song if the user has made it a favorite", function() {
-//     spyOn(song, 'persistFavoriteStatus');
-//
-//     player.play(song);
-//     player.makeFavorite();
-//
-//     expect(song.persistFavoriteStatus).toHaveBeenCalledWith(true);
-//   });
-//
-//   //demonstrates use of expected exceptions
-//   describe("#resume", function() {
-//     it("should throw an exception if song is already playing", function() {
-//       player.play(song);
-//
-//       expect(function() {
-//         player.resume();
-//       }).toThrowError("song is already playing");
-//     });
-//   });
-// });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,6 +1,47 @@
+debugger
+
 function Thermostat() {
-  this.temperature = 20
-}
+  this.MINIMUM_TEMPERATURE = 10;
+  this.temperature = 20;
+  this.psModeState = true;
+};
+
+Thermostat.prototype.isMinimumTemperature = function () {
+  return this.temperature === this.MINIMUM_TEMPERATURE;
+};
+
+Thermostat.prototype.currentTemperature = function() {
+  return this.temperature;
+};
+
+Thermostat.prototype.up = function() {
+  if (this.psModeState === false && this.temperature >= 32) {
+    this.temperature = 32;
+  } else if (this.psModeState === true && this.temperature >= 25) {
+    this.temperature = 25;
+  } else {
+    this.temperature += 1
+  }
+};
+
+Thermostat.prototype.down = function() {
+  if (this.isMinimumTemperature()) {
+    return;
+  }
+  this.temperature -= 1
+};
+
+Thermostat.prototype.psMode = function() {
+  return this.psModeState;
+};
+
+Thermostat.prototype.switch = function() {
+  if (this.psModeState === true) {
+    this.psModeState = false;
+  } else {
+    this.psModeState = true;
+  }
+};
 
 // Thermostat.prototype.temperature = function() {
 //   return 20;
