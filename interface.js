0bba90d377fceac2360ef9c69a7ba4c0f774b9d7
Riya Dattani <riya_dattani@hotmail.com>
2019-04-05 14:54:43 +0100

    app can display the weather of any city using an API

--- interface.js
+++ interface.js
@@ -36,4 +36,22 @@ $(document).ready(function() {
       $("#power-saving-button").text("on")
     }
   };
+
+  displayWeather('London');
+
+  $('#select-city').submit(function(event) {
+    event.preventDefault();
+    var city = $('#current-city').val();
+    displayWeather(city);
+  })
+
+  function displayWeather(city) {
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+    var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed';
+    var units = '&units=metric';
+    $.get(url + token + units, function(data) {
+      $('#current-temperature').text(data.main.temp);
+      $('#city').text(data.name);
+    })
+  }
 })
